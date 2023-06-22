e3a99684a41de2600816e46b40c0b04605ad4b56
alask-code <anonimoplayer736@gmail.com>
2023-06-22 04:22:22 -0300

    first comit

---
+++ script.js
@@ -0,0 +1,26 @@
+const toggleIcon = document.querySelector('.icon')
+const content = document.querySelector('.card')
+var darkmode = false
+function toggleDark() {
+    if (darkmode == false) {
+        toggleIcon.style.opacity = "0"
+        setTimeout(() => {
+            toggleIcon.classList.remove('fa-moon')
+            toggleIcon.classList.add('fa-sun')
+            toggleIcon.style.opacity = "1"
+        }, 250)
+
+        content.classList.add('dark')
+        darkmode = true
+    } else {
+        toggleIcon.style.opacity = "0"
+        setTimeout(() => {
+            toggleIcon.classList.remove('fa-sun')
+            toggleIcon.classList.add('fa-moon')
+            toggleIcon.style.opacity = "1"
+        }, 250)
+
+        content.classList.remove('dark')
+        darkmode = false
+    }
+}
