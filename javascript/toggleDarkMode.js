07f2d0acd5907f65a203e989b05e55e835750e84
alask-code <anonimoplayer736@gmail.com>
2023-06-23 15:46:22 -0300

    full icon coloring

--- javascript/toggleDarkMode.js
+++ javascript/toggleDarkMode.js
@@ -3,9 +3,13 @@ function toggleDark() {
     document.body.classList.toggle('dark-mode')
     if (!darkmode) {
         darkmode = true
+        const darkModeText = document.querySelector('.dark-mode-text')
         const toggleDarkModeIcon = document.querySelector('.toggle-dark-mode')
         toggleDarkModeIcon.style.opacity = "0"
+        darkModeText.style.opacity = "0"
         setTimeout(() => {
+            darkModeText.innerHTML = "Modo Claro"
+            darkModeText.style.opacity = "1"
             toggleDarkModeIcon.classList.remove('fa-moon')
             toggleDarkModeIcon.classList.add('fa-sun')
             toggleDarkModeIcon.style.opacity = "1"
@@ -13,9 +17,13 @@ function toggleDark() {
     }
     else {
         darkmode = false
+        const darkModeText = document.querySelector('.dark-mode-text')
         const toggleDarkModeIcon = document.querySelector('.toggle-dark-mode')
         toggleDarkModeIcon.style.opacity = "0"
+        darkModeText.style.opacity = "0"
         setTimeout(() => {
+            darkModeText.innerHTML = "Modo Noturno"
+            darkModeText.style.opacity = "1"
             toggleDarkModeIcon.classList.remove('fa-sun')
             toggleDarkModeIcon.classList.add('fa-moon')
             toggleDarkModeIcon.style.opacity = "1"
